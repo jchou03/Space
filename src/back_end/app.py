fd43bfc4bddb80b77dd51336a0a64b5dd61d5514
jchou21 <jared.chou7@gmail.com>
2023-10-31 23:53:15 -0400

    created back end to send journal prompts to front end

---
+++ src/back_end/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+journal_prompts=[
+    "How would you spend your perfect day off? What makes that perfect for you?",
+    "Describe yourself in ten words. Why do those words come to mind?",
+    "What makes you feel the most inspired?",
+    "What is your favorite form of self-care? Why?",
+    "What can you do today to take better care of yourself?",
+    "What comes to mind first when you think of what makes you feel safe?",
+    "What are five things about yourself you want people to know?",
+    "What’s something that makes you feel warm inside?",
+    "Explain what’s hardest for you in as many words as it takes.",
+    "If you were to improve something about your life, what would that be? Why?",
+    "What’s your first coping mechanism that comes to mind? Do you think it’s helpful or harmful, and why?"
+]
+
+@app.route('/api/journal_prompt', methods=['GET'])
+def get_journal_prompt():
+    import random
+    prompt = random.choice(journal_prompts)
+    return jsonify({'prompt':prompt})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
--- src/front_end/space/src/components/Journal.js
+++ src/front_end/space/src/components/Journal.js
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/Journal.css';
 
 const Journal = () => {
     const [entry, setEntry] = useState('');
-    const prompt = "What's the most memorable thing that happened to you today?"; // Example prompt
+    const [prompt, setPrompt] = useState('Loading...');
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/api/journal_prompt')
+            .then(response => {
+                if(!response.ok){
+                    throw new Error('Network response was not ok')
+                }
+                return response.json()
+            })
+            .then(data => {
+                setPrompt(data.prompt)
+            })
+            .catch(error => {
+                console.error("There was a problem with fetching the journal prompt. Error message: " + error.message)
+                setPrompt("What's the most memorable thing that happened to you today?")
+            })
+    })
 
     return (
         <div className="journal-container">
